b2f5dc77267ceb8717f620f6cf5b311428fd4d92
Alberto Hernandez Ceezo <AlbertoHdezCerezo@users.noreply.github.com>
2023-05-07 16:20:21 +0200

    Ship working

--- resources/ShipStats.gd
+++ resources/ShipStats.gd
@@ -4,8 +4,10 @@ class_name ShipStats extends Resource
 @export var rotation_speed: float
 @export var acceleration: float
 @export var max_speed: float
+@export var friction: float
 
-func _init(p_rotation_speed: float = 0, p_acceleration: float = 0, p_max_speed: float = 0):
+func _init(p_rotation_speed: float = 0, p_acceleration: float = 0, p_max_speed: float = 0, p_friction: float = 0):
 	rotation_speed = p_rotation_speed
 	acceleration = p_acceleration
 	max_speed = p_max_speed
+	friction = p_friction
